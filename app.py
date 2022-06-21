3e5d41452b25bcdf778a55f099eb925f9a487f8b
rramadhani015 <ramadhani015@gmail.com>
2022-06-21 21:52:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def uploader_file():
       f.save(os.path.join(app.config['UPLOAD_FOLDER'],file_name))
       #f.save(secure_filename(f.filename))
       #return 'file uploaded successfully'
-      # link = os.path.join(app.config['UPLOAD_FOLDER'],file_name)
-      link = "http://103.56.148.64:5000/parse/".secure_filename(f.filename)
+      name = os.path.join(app.config['UPLOAD_FOLDER'],file_name)
+      link = "http://103.56.148.64:5000/parse/"+name
       html="""<html>
          <body>
             <a href="?file={}" target="blank"> Parse PDF</a> 
